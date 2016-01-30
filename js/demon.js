4d0bb1a3b4eb9cd5bc839c6e72d0864b622a0177
Brad K. Harms <brad.k.harms@gmail.com>
2016-01-29 23:21:04 -0600

    starting to reload

---
+++ js/demon.js
@@ -0,0 +1,23 @@
+var Demon = function Demon(game, x, y) {
+  Phaser.Sprite.call(this, game, x, y, 'demon', 0);
+  this.anchor.setTo(.5, .5);
+  this.animations.add('down', [0], 0);
+  this.animations.add('left', [1], 0);
+  this.animations.add('right', [2], 0);
+  this.animations.add('up', [3], 0);
+  game.physics.arcade.enable(this);
+};
+
+Demon.preload = function preload(game) {
+  game.load.spritesheet('demon', '/img/demon_4facing.png', 64, 64);
+};
+
+Demon.prototype = Object.create(Phaser.Sprite.prototype, {
+  constructor: Demon,
+
+  // update: function update() {
+  //   Sprite.prototype.update.call(this);
+  //
+  //
+  // }
+});
